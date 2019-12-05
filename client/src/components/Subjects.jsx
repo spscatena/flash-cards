d4a50a6057e2f5061a7c171aeb1661d9a130c4cd
Sabrina Scatena <sabrina.scatena@gmail.com>
2019-12-04 21:50:18 -0500

    some css

--- client/src/components/Subjects.jsx
+++ client/src/components/Subjects.jsx
@@ -5,30 +5,28 @@ export default class Subjects extends Component {
   constructor(props) {
     super(props)
     this.state = {
-     
-     
+
     }
   }
-  
-
-  
 
   render() {
     return (
       <div>
-
-        
-        {this.props.subjects.map(subject => (
-          <div key={subject.id}>
-            <p>{subject.title}</p>
-            <p>{subject.description}</p>
-            <button onClick={()=>this.props.handleSubjectDelete(subject.id)}>Delete</button>
-          </div>))}
         <SubjectCreate
           handleChange={this.props.handleChange}
           handleSubjectSubmit={this.props.handleSubjectSubmit}
           subjectData={this.props.subjectData}
         />
+
+        <div id="subject-container">
+          {this.props.subjects.map(subject => (
+
+            <div id="subject-card" key={subject.id}>
+              <p>{subject.title}</p>
+              <p>{subject.description}</p>
+              <button onClick={() => this.props.handleSubjectDelete(subject.id)}>Delete</button>
+            </div>))}
+        </div>
       </div>
     )
   }
